041f71a36cfc77cf2b75ca38844452aa36029dde
Harianto van Insulinde <hariantoatwork@gmail.com>
2021-01-05 14:33:36 +0100

    Change to MySQL

--- build/dovecot/docker-entrypoint.sh
+++ build/dovecot/docker-entrypoint.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-until ping -c 3 postgres 2>1 > /dev/null; do
-  # echo "Postgres is unavailable - sleeping"
+until ping -c 3 $DOVECOT_DB_HOST 2>1 > /dev/null; do
+  echo "$DOVECOT_DB_HOST is unavailable - sleeping"
   sleep 1
 done
 
-while [ ! -f /etc/ssl/certs/imap.$DOMAIN.crt ]
+while [ ! -f /etc/ssl/certs/imap.$DOVECOT_DOMAIN.crt ]
 do
   sleep 1
 done
 
 # Update db credentials
-sed -i "s/ndb/$DB_NAME/g" /etc/dovecot/dovecot-*sql.conf.ext
-sed -i "s/usrname/$USER_NAME/g" /etc/dovecot/dovecot-*sql.conf.ext
-sed -i "s/passwd/$USER_PASSWD/g" /etc/dovecot/dovecot-*sql.conf.ext
+sed -i "s/DOVECOT_DB_HOST/$DOVECOT_DB_HOST/g" /etc/dovecot/dovecot-*sql.conf.ext
+sed -i "s/DOVECOT_DB_NAME/$DOVECOT_DB_NAME/g" /etc/dovecot/dovecot-*sql.conf.ext
+sed -i "s/DOVECOT_DB_USER/$DOVECOT_DB_USER/g" /etc/dovecot/dovecot-*sql.conf.ext
+sed -i "s/DOVECOT_DB_PASSWORD/$DOVECOT_DB_PASSWORD/g" /etc/dovecot/dovecot-*sql.conf.ext
 # Update domain
-sed -i "s/example.com/$DOMAIN/g" /etc/dovecot/conf.d/10-ssl.conf
-sed -i "s/example.com/$DOMAIN/g" /etc/dovecot/dovecot.conf
+sed -i "s/example.com/$DOVECOT_DOMAIN/g" /etc/dovecot/conf.d/10-ssl.conf
+sed -i "s/example.com/$DOVECOT_DOMAIN/g" /etc/dovecot/dovecot.conf
 
 # Run dovecot
 exec "$@"
